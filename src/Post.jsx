655e7b736777bc6e9dd47927af23bb886f94e2bd
AlanJhonatan <alan.amorim.franca@gmail.com>
2023-04-08 23:38:26 +0200

    feat: add Post basic html

--- src/Post.jsx
+++
@@ -1,5 +0,0 @@
-export function Post() {
-    return (
-        <h2>Here lies an post</h2>
-    )
-}
---
+++ src/components/Post.jsx
@@ -0,0 +1,27 @@
+import styles from './Post.module.css'
+
+export function Post() {
+    return (
+        <article className={styles.post}>
+            <header>
+                <div className={styles.author}>
+                    <img className={styles.avatar} src="https://github.com/alanjhonatan.png" alt="Profile image" />
+                    <div className={styles.authorInfo}>
+                        <strong>Alan França</strong>
+                        <span>Web Developer</span>
+                    </div>
+                </div>
+
+                <time title='08 de Abril às 22:34' dateTime='2023-05-08 22:34'>Publicado há 1h</time>
+            </header>
+
+            <div className={styles.content}>
+                <p>Fala galeraa 👋</p>
+                <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
+                <p>👉 <a href="">jane.design/doctorcare</a></p>
+                <p><a href="">#novoprojeto #nlw #rocketseat</a></p>
+            </div>
+
+        </article>
+    )
+}
