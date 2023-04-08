e17a32b1fc63a0d54f9c5cbd8c67f1f1c61ddce9
AlanJhonatan <alan.amorim.franca@gmail.com>
2023-04-08 23:39:19 +0200

    feat: add alt image attr

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -8,7 +8,7 @@ export function Sidebar() {
             <img className={styles.cover} src="https://images.unsplash.com/photo-1680967230555-a803660575dc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=50" alt="User cover image" />
 
             <div className={styles.profile}>
-                <img className={styles.avatar} src="https://github.com/alanjhonatan.png" alt="" />
+                <img className={styles.avatar} src="https://github.com/alanjhonatan.png" alt="Profile image" />
                 
                 <strong>Alan França</strong>
                 <span>Web Developer</span>
