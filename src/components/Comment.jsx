83bd19deb9be4bb4e298df9a105f8d947686584a
AlanJhonatan <alan.amorim.franca@gmail.com>
2023-04-09 00:30:51 +0200

    feat: add basic Comment component structure

---
+++ src/components/Comment.jsx
@@ -0,0 +1,32 @@
+import { ThumbsUp, Trash } from 'phosphor-react'
+import styles from './Comment.module.css'
+
+export function Comment() {
+    return (
+        <div className={styles.comment}>
+            <img className={styles.avatar} src="https://github.com/alanjhonatan.png" alt="Profile image" />
+
+            <div className={styles.commentBox}>
+                <div className={styles.commentContent}>
+                    <header>
+                        <div className={styles.authorAndTime}>
+                            <strong>Alan França</strong>
+                            <time title='08 de Abril às 22:34' dateTime='2023-05-08 22:34'>Cerca de 1h atrás</time>   
+                        </div>
+
+                        <button title='Deletar comentário'>
+                            <Trash size={20} />
+                        </button>
+                    </header>
+
+                    <p>Muito bom ! Parabéns !!!</p>
+                </div>
+
+                <footer>
+                    <ThumbsUp />
+                    Aplaudir <span>20</span>
+                </footer>
+            </div>
+        </div>
+    )
+}
