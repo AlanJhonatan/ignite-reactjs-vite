99872ae98e6808b4bc5b9813d773cd2af09b3fc1
AlanJhonatan <alan.amorim.franca@gmail.com>
2023-04-08 23:07:21 +0200

    feat: add Sidebar component with basic styles

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { Post } from './Post';
 
 import styles from './App.module.css';
 import './global.css';
+import { Sidebar } from "./components/Sidebar";
 
 export function App() {
   return (
@@ -10,9 +11,7 @@ export function App() {
       <Header />
       
       <div className={styles.wrapper}>
-        <aside>
-          sidebar
-        </aside>
+        <Sidebar />
         
         <main>
           <Post />
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,20 @@
+import styles from './Sidebar.module.css'
+
+export function Sidebar() {
+    return (
+        <aside className={styles.sidebar}>
+            <img className={styles.cover} src="https://images.unsplash.com/photo-1680967230555-a803660575dc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=50" alt="User cover image" />
+
+            <div className={styles.profile}>
+                <strong>Alan França</strong>
+                <span>Web Developer</span>
+            </div>
+
+            <footer>
+                <a href="#">
+                    Editar seu perfil
+                </a>
+            </footer>
+        </aside>
+    )
+}
