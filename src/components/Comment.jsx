dd6ebbd213117de752d1af98158f58fe3fa61608
AlanJhonatan <alan.amorim.franca@gmail.com>
2023-04-11 00:24:47 +0200

    feat: add text prop in component

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -2,7 +2,7 @@ import { ThumbsUp, Trash } from 'phosphor-react'
 import { Avatar } from './Avatar'
 import styles from './Comment.module.css'
 
-export function Comment() {
+export function Comment({ text }) {
     return (
         <div className={styles.comment}>
             <Avatar src="https://github.com/alanjhonatan.png" />
@@ -19,7 +19,7 @@ export function Comment() {
                         </button>
                     </header>
 
-                    <p>Muito bom ! Parabéns !!!</p>
+                    <p>{text}</p>
                 </div>
 
                 <footer>
